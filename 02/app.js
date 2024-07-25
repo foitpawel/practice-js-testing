e33af37dfaafc9488208fddd1c087f9cea558c35
fawelp <foitpawel@gmail.com>
2024-07-25 09:09:31 +0200

    Catch the error

--- 02/app.js
+++ 02/app.js
@@ -1,28 +1,42 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const clickEl = document.querySelector('.error--click');
-    const enterEl = document.querySelector('.error--enter');
+  const clickEl = document.querySelector('.error--click');
+  const enterEl = document.querySelector('.error--enter');
 
-    setRandomPosition(clickEl);
-    setRandomPosition(enterEl);
+  setRandomPosition(clickEl);
+  setRandomPosition(enterEl);
 
-    initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
-    initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
+  initEventWithError(clickEl, 'click', new RangeError('Błąd zakresu!'));
+  initEventWithError(enterEl, 'mouseenter', new TypeError('Błąd typu!'));
 
 }
 
 function setRandomPosition(element, error = null) {
-    element.style.top = Math.random() * 600 + 'px';
-    element.style.left = Math.random() * 800 + 'px';
+  element.style.top = Math.random() * 600 + 'px';
+  element.style.left = Math.random() * 800 + 'px';
 
-    if(error) {
-        throw error;
-    }
+  if(error) {
+      throw error;
+  }
 }
 
 function initEventWithError(element, eventName, error) {
-    element.addEventListener(eventName, function() {
-        setRandomPosition(this, error);
-    })
+  const alertTxt = document.querySelector('.alert__message');
+  element.addEventListener(eventName, function() {
+  try{setRandomPosition(this, error)
+  } catch(error) {
+    alertTxt.innerHTML = error.message;
+    document.querySelector('.alert--hidden').style.display = 'flex';
+  } 
+  // finally {
+  //   document.body.addEventListener('click', (function hideAlert(event){
+  //     if (!event.target.closest('.alert--hidden')) {
+  //     document.querySelector('.alert--hidden').style.display = 'none'
+  //     }
+  //     document.body.removeEventListener('click', hideAlert);
+  //   }));
+  // }
+  })
+
 }
