522156b22515a3d3238a1c440538d231de5b4677
fawelp <foitpawel@gmail.com>
2024-07-25 09:46:38 +0200

    Add first test

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,3 @@
-export default function randomNumber(min, max) {
-
+export default function randomNumber([min, max]) {
+  return([min, max]);
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,7 @@
 import randomNumber from './app';
+
+test('Losowanie dowolnej liczby z podanego przedziału', () => 
+  expect (
+    randomNumber([1, 10])
+  ).toEqual([1, 10])
+)
