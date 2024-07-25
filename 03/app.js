f5b50e9bc515604662f87f5649410d6b444fba4f
fawelp <foitpawel@gmail.com>
2024-07-25 15:57:35 +0200

    Implement the function

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,6 @@
-export default function randomNumber([min, max]) {
-  return([min, max]);
+export default function randomNumber(min, max) {
+  if(max >= min) {
+    return Math.floor((Math.random() * (max - min + 1) + min));
+  }
+  else {console.log('Niepoprawny zestaw danych')}
 }
