cbfd9b0d8c8e496cd25d7065d46e961989c053f9
fawelp <foitpawel@gmail.com>
2024-07-25 17:24:39 +0200

    Implement class User

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,30 @@
 export default class User {
+  constructor({email, password}) {
+    this.email=email;
+    this.password=password;
+    this.validateEmail();
+    this.validatePassword();
+    this.login();
+  }
 
-}
+  getEmail() {return this.email}
+  getPassword() {return this.password}
+
+  validateEmail() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailRegex.test(this.email)) {
+      throw new Error('Invalid email address');
+    }
+  }
+
+  validatePassword() {
+    if(this.password.length < 4) {
+      throw new Error('Invalid password');
+    }
+  }
+
+  login() {
+    if(this.email.includes('devmentor.pl')) return true
+    else return false
+  }
+};
